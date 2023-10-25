c15cbb6d92f17f4b544e692375ae38686c28c99c
ostluc <osta@netcloud.ch>
2023-10-25 21:38:07 +0200

    CategoryForm = False

--- src/app/category/category-modal/category-modal.component.ts
+++ src/app/category/category-modal/category-modal.component.ts
@@ -2,16 +2,30 @@ import { Component } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { ActionSheetService } from '../../shared/service/action-sheet.service';
 import { filter, from } from 'rxjs';
+import { FormBuilder, FormGroup } from '@angular/forms'; // Import für FormGroup hinzufügen
 
 @Component({
   selector: 'app-category-modal',
   templateUrl: './category-modal.component.html',
 })
 export class CategoryModalComponent {
+  // Fügen Sie das categoryForm und submitting hinzu
+  readonly categoryForm: FormGroup;
+  submitting = false;
+
   constructor(
     private readonly actionSheetService: ActionSheetService,
     private readonly modalCtrl: ModalController,
-  ) {}
+    private fb: FormBuilder // Fügen Sie FormBuilder für die Verwendung von FormGroup hinzu
+  ) {
+    // Initialisieren Sie das categoryForm im Konstruktor
+    this.categoryForm = this.fb.group({
+      // Hier können Sie die Formularfelder und Validierungen hinzufügen
+      // Beispiel:
+      // categoryName: ['', Validators.required],
+      // ...
+    });
+  }
 
   cancel(): void {
     this.modalCtrl.dismiss(null, 'cancel');
